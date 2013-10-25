4644224c7e074056e45bb91427f05e94080e5cd2
jeremyBass <jeremybass@cableone.net>
2013-10-25 08:56:50 -0700

    some refinments

---
+++ skin/frontend/wsu_base/default/js/controller.js
@@ -0,0 +1,22 @@
+(function($){
+	$(function(){
+		var slideshow = $( '.cycle-slideshow' );
+		var progress = $('#progress');
+
+		slideshow.on( 'cycle-initialized cycle-before', function( e, opts ) {
+			progress.stop(true).css( 'width', 0 );
+		});
+		
+		slideshow.on( 'cycle-initialized cycle-after', function( e, opts ) {
+			if ( ! slideshow.is('.cycle-paused') )
+				progress.animate({ width: '100%' }, opts.timeout, 'linear' );
+		});
+		
+		slideshow.on( 'cycle-paused', function( e, opts ) {
+		   progress.stop(); 
+		});
+		slideshow.on( 'cycle-resumed', function( e, opts, timeoutRemaining ) {
+			progress.animate({ width: '100%' }, timeoutRemaining, 'linear' );
+		});
+	});
+})(jQuery);
