ee6eb1cb7c1b0d4528113aa1c6d0c48ece6a5409
jeremyBass <jeremybass@cableone.net>
2013-11-19 09:42:10 -0800

    corrections

--- skin/frontend/wsu_base/default/js/controller.js
+++ skin/frontend/wsu_base/default/js/controller.js
@@ -20,3 +20,53 @@
 		});
 	});
 })(jQuery);
+
+
+//	//from app\design\frontend\wsu_base\default\template\catalog\product\view.phtml
+//	var productAddToCartForm = new VarienForm('product_addtocart_form');
+//	productAddToCartForm.submit = function(button, url) {
+//		if (this.validator.validate()) {
+//			var form = this.form;
+//			var oldUrl = form.action;
+//	
+//			if (url) {
+//			   form.action = url;
+//			}
+//			var e = null;
+//			try {
+//				this.form.submit();
+//			} catch (e) {
+//			}
+//			this.form.action = oldUrl;
+//			if (e) {
+//				throw e;
+//			}
+//	
+//			if (button && button != 'undefined') {
+//				button.disabled = true;
+//			}
+//		}
+//	}.bind(productAddToCartForm);
+//	
+//	productAddToCartForm.submitLight = function(button, url){
+//		if(this.validator) {
+//			var nv = Validation.methods;
+//			delete Validation.methods['required-entry'];
+//			delete Validation.methods['validate-one-required'];
+//			delete Validation.methods['validate-one-required-by-name'];
+//			// Remove custom datetime validators
+//			for (var methodName in Validation.methods) {
+//				if (methodName.match(/^validate-datetime-.*/i)) {
+//					delete Validation.methods[methodName];
+//				}
+//			}
+//	
+//			if (this.validator.validate()) {
+//				if (url) {
+//					this.form.action = url;
+//				}
+//				this.form.submit();
+//			}
+//			Object.extend(Validation.methods, nv);
+//		}
+//	}.bind(productAddToCartForm);
