7d2f2f44920bfcca45cb9bde57242e1f97f6eedd
Jeremy Bass <jeremybass@cableone.net>
2014-02-03 17:20:10 -0800

    sets up the spine color options

--- app/code/local/Wsu/Wsu/Model/System/Config/Backend/Design/Color/Wsubrand.php
+++ app/code/local/Wsu/Wsu/Model/System/Config/Backend/Design/Color/Wsubrand.php
@@ -3,16 +3,16 @@
 class Wsu_Wsu_Model_System_Config_Source_Design_Color_Wsubrand{
     public function toOptionArray(){
 		return array(
-			array('value' => 'Default', 'label' => Mage::helper('wsu_themecontrol')->__('Default [white]')),
-			array('value' => 'Lightest', 'label' => Mage::helper('wsu_themecontrol')->__('Lightest [grey]')),
-			array('value' => 'Lighter', 'label' => Mage::helper('wsu_themecontrol')->__('Lighter [grey]')),
-			array('value' => 'Light', 'label' => Mage::helper('wsu_themecontrol')->__('Light [grey]')),
-			array('value' => 'Gray', 'label' => Mage::helper('wsu_themecontrol')->__('Gray [grey]')),
-			array('value' => 'Dark', 'label' => Mage::helper('wsu_themecontrol')->__('Dark [grey]')),
-			array('value' => 'Darker', 'label' => Mage::helper('wsu_themecontrol')->__('Darker [grey]')),
-			array('value' => 'Darkest', 'label' => Mage::helper('wsu_themecontrol')->__('Darkest [grey]')),
-			array('value' => 'Crimson', 'label' => Mage::helper('wsu_themecontrol')->__('Crimson')),
-			array('value' => 'Transparent', 'label' => Mage::helper('wsu_themecontrol')->__('Transparent'))
+			array('value' => 'default', 'label' => Mage::helper('wsu_themecontrol')->__('Default [white]')),
+			array('value' => 'lightest', 'label' => Mage::helper('wsu_themecontrol')->__('Lightest [grey]')),
+			array('value' => 'lighter', 'label' => Mage::helper('wsu_themecontrol')->__('Lighter [grey]')),
+			array('value' => 'light', 'label' => Mage::helper('wsu_themecontrol')->__('Light [grey]')),
+			array('value' => 'gray', 'label' => Mage::helper('wsu_themecontrol')->__('Gray [grey]')),
+			array('value' => 'dark', 'label' => Mage::helper('wsu_themecontrol')->__('Dark [grey]')),
+			array('value' => 'darker', 'label' => Mage::helper('wsu_themecontrol')->__('Darker [grey]')),
+			array('value' => 'darkest', 'label' => Mage::helper('wsu_themecontrol')->__('Darkest [grey]')),
+			array('value' => 'crimson', 'label' => Mage::helper('wsu_themecontrol')->__('Crimson')),
+			array('value' => 'transparent', 'label' => Mage::helper('wsu_themecontrol')->__('Transparent'))
         );
     }
 }
