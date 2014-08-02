7859924c13999cbbb5318a0bdf6ae434815df697
Jeremy Bass <jeremybass@cableone.net>
2014-08-02 16:49:49 -0700

    provides a list of spine defaults

---
+++ app/code/local/Wsu/Themecontrol/Model/System/Config/Source/Spine/Social.php
@@ -0,0 +1,16 @@
+<?php
+class Wsu_Themecontrol_Model_System_Config_Source_Spine_Social{
+    public function toOptionArray(){
+		return array(
+			array('value' => 'facebook',			'label' => Mage::helper('wsu_themecontrol')->__('facebook')),
+			array('value' => 'twitter',		'label' => Mage::helper('wsu_themecontrol')->__('twitter')),
+			array('value' => 'youtube',		'label' => Mage::helper('wsu_themecontrol')->__('youtube')),
+			array('value' => 'directory',		'label' => Mage::helper('wsu_themecontrol')->__('directory')),
+			array('value' => 'linkedin',		'label' => Mage::helper('wsu_themecontrol')->__('linkedin')),
+			array('value' => 'pinterest',		'label' => Mage::helper('wsu_themecontrol')->__('pinterest')),
+			array('value' => 'gplus',		'label' => Mage::helper('wsu_themecontrol')->__('gplus')),
+			array('value' => 'flickr',		'label' => Mage::helper('wsu_themecontrol')->__('flickr')),
+			array('value' => 'tumblr',		'label' => Mage::helper('wsu_themecontrol')->__('tumblr')),
+        );
+    }
+}
