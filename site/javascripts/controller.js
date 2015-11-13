0b26bd174445a7359414c47b5a71aaf7fc58e705
Jeremy Bass <jeremybass@cableone.net>
2015-11-13 12:32:05 -0800

    adjusts the light zoom to always show fully in the view port

--- site/javascripts/controller.js
+++ site/javascripts/controller.js
@@ -1,38 +1,46 @@
- var timeout = null;
+	var timeout = null;
+	$(document).on('mousemove', function() {
+		 setOnMove($('.img_zoom:hover'));
+	});
+	function setOnMove(target){
+		clearTimeout(window.timeout);
+		window.timeout = setTimeout(function() {
+			
+			if( (!target.is('.closed') || !target.is('.transition')) && target.is(':hover') ){
+				clearTimeout(window.timeout);
+				target.addClass('transition');
+			}
+		}, 100);
+	}
+
   $(document).ready(function(){
 	$('.wrapper.markdown-body img').wrap('<div class="img_zoom">');
+	$('.wrapper.markdown-body .img_zoom').wrap('<div class="img_zoom_holder">');
+	$(window).resize(function(){
+		$.each($('.img_zoom_holder'),function(){
+			$(this).css( 'height', $(this).find('img').height() );
+		});
+	}).trigger('resize');
+	
     $('.img_zoom').hover(function() {
-		var target = $(this);
-
-		$(document).on('mousemove', function() {
-			clearTimeout(timeout);
-			timeout = setTimeout(function() {
-				if( (!target.is('.closed') || !target.is('.transition')) && target.is(':hover') ){
-					target.addClass('transition');
-				}
-			}, 50);
-		});		
-		
+		//var target = $(this);	
+		//setOnMove(target);	
     }, function() {
-		clearTimeout(timeout);
+		clearTimeout(window.timeout);
         $(this).removeClass('transition');
 		$(this).removeClass('closed');
     });
-	$('.img_zoom,.img_zoom:before').on('click',function(){
-
-	});
 	$('.img_zoom').on('click',function(){
-		if( !$(this).is('.closed') && !$(this).is('.transition') ){
+		if( 
+			( !$(this).is('.closed') && !$(this).is('.transition') )
+			||
+			( !$(this).is('.transition') && $(this).closest('.img_zoom_holder').has('.closed') )
+		){
 			$(this).addClass('transition');
 			$(this).removeClass('closed');
 		}else{
-			if( !$(this).is('.transition') ){
-				$(this).addClass('transition');
-				$(this).removeClass('closed');
-			}else{
-				$(this).removeClass('transition');
-				$(this).addClass('closed');
-			}
+			$(this).removeClass('transition');
+			$(this).addClass('closed');
 		}
 	});
 	
