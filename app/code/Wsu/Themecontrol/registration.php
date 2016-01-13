2cb767028eb38aedde265ff44ae14081583c2bdc
Jeremy Bass <jeremybass@cableone.net>
2016-01-13 14:54:45 -0800

    updates to get module from Mage 1 ro 2

---
+++ app/code/Wsu/Themecontrol/registration.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @author jeremyBass
+ * @copyright Copyright (c) 2015 WSU (https://wsu.edu)
+ * @package Wsu_Themecontrol
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Wsu_Themecontrol',
+    __DIR__
+);
