cbe294dc4af42916d348cb00913cf8e6c6e984df
Jeremy Bass <jeremybass@cableone.net>
2016-02-23 12:21:13 -0800

    switch out to the non-ruby version of the sass

--- skin/frontend/wsu_base/default/Gruntfile.js
+++ skin/frontend/wsu_base/default/Gruntfile.js
@@ -21,6 +21,9 @@ module.exports = function(grunt) {
 			tasks: [ 'sass', 'concat', 'jshint', 'env:dev', 'autoprefixer', 'cssmin', 'uglify' , 'copy:maps' ]
 		},
 		sass: {	
+			options: {
+				sourceMap: true
+			},
 			dev: {
 				files: [
 					{ src: "src/scss/theme.scss", dest: "build/_pre_sass/theme.css" },
@@ -135,7 +138,7 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-autoprefixer');
-	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-sass');
 	// Default task(s).
 	grunt.registerTask('start', ['watch']);
 	grunt.registerTask('default', ['jshint']);
