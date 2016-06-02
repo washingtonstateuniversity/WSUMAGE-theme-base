280e3830a8f7bd77529551308ab2d0d5d83bd075
Jeremy Bass <jeremybass@cableone.net>
2016-06-01 21:00:14 -0700

    do the replacement of spectrum

--- app/code/local/Wsu/Themecontrol/Block/Adminhtml/System/Config/Form/Field/Color.php
+++ app/code/local/Wsu/Themecontrol/Block/Adminhtml/System/Config/Form/Field/Color.php
@@ -8,31 +8,61 @@ class Wsu_Themecontrol_Block_Adminhtml_System_Config_Form_Field_Color extends Ma
      */
     protected function _getElementHtml(Varien_Data_Form_Element_Abstract $element) {
         $html   = $element->getElementHtml(); //Default HTML
-        $jsPath = $this->getJsUrl('wsu/jquery/jquery-1.7.2.min.js');
-        $mcPath = $this->getJsUrl('wsu/jquery/plugins/mcolorpicker/');
-        if (Mage::registry('jqueryLoaded') == false) {
-            $html .= '
-			<script type="text/javascript" src="' . $jsPath . '"></script>
-			<script type="text/javascript">jQuery.noConflict();</script>
-			';
-            Mage::register('jqueryLoaded', 1);
-        }
+		
         if (Mage::registry('colorPickerLoaded') == false) {
             $html .= '
-			<script type="text/javascript" src="' . $mcPath . 'mcolorpicker.min.js"></script>
-			<script type="text/javascript">
-				jQuery.fn.mColorPicker.init.replace = false;
-				jQuery.fn.mColorPicker.defaults.imageFolder = "' . $mcPath . 'images/";
-				jQuery.fn.mColorPicker.init.allowTransparency = true;
-				jQuery.fn.mColorPicker.init.showLogo = false;
-			</script>
-            ';
+			<script type="text/javascript" src="/js/wsu/spectrum/js/spectrum.js"></script>
+			<link rel="stylesheet" type="text/css"  href="/js/wsu/spectrum/css/spectrum.css" media="all"/> 
+			';
             Mage::register('colorPickerLoaded', 1);
         }
         $html .= '
 			<script type="text/javascript">
 				jQuery(function($){
-					$("#' . $element->getHtmlId() . '").attr("data-hex", true).width("250px").mColorPicker();
+					$("#' . $element->getHtmlId() . '").spectrum({
+						color: "#ECC",
+						showInput: true,
+						showAlpha:true,
+						className: "full-spectrum",
+						showInitial: true,
+						showPalette: true,
+						showSelectionPalette: true,
+						maxSelectionSize: 10,
+						preferredFormat: "hex",
+						localStorageKey: "spectrum.wsumage",
+						move: function (color) {
+							
+						},
+						show: function () {
+						
+						},
+						beforeShow: function () {
+						
+						},
+						hide: function () {
+						
+						},
+						change: function() {
+							
+						},
+						palette: [
+							["rgb(0, 0, 0)", "rgb(67, 67, 67)", "rgb(102, 102, 102)",
+							"rgb(204, 204, 204)", "rgb(217, 217, 217)","rgb(255, 255, 255)"],
+							["rgb(152, 0, 0)", "rgb(255, 0, 0)", "rgb(255, 153, 0)", "rgb(255, 255, 0)", "rgb(0, 255, 0)",
+							"rgb(0, 255, 255)", "rgb(74, 134, 232)", "rgb(0, 0, 255)", "rgb(153, 0, 255)", "rgb(255, 0, 255)"], 
+							["rgb(230, 184, 175)", "rgb(244, 204, 204)", "rgb(252, 229, 205)", "rgb(255, 242, 204)", "rgb(217, 234, 211)", 
+							"rgb(208, 224, 227)", "rgb(201, 218, 248)", "rgb(207, 226, 243)", "rgb(217, 210, 233)", "rgb(234, 209, 220)", 
+							"rgb(221, 126, 107)", "rgb(234, 153, 153)", "rgb(249, 203, 156)", "rgb(255, 229, 153)", "rgb(182, 215, 168)", 
+							"rgb(162, 196, 201)", "rgb(164, 194, 244)", "rgb(159, 197, 232)", "rgb(180, 167, 214)", "rgb(213, 166, 189)", 
+							"rgb(204, 65, 37)", "rgb(224, 102, 102)", "rgb(246, 178, 107)", "rgb(255, 217, 102)", "rgb(147, 196, 125)", 
+							"rgb(118, 165, 175)", "rgb(109, 158, 235)", "rgb(111, 168, 220)", "rgb(142, 124, 195)", "rgb(194, 123, 160)",
+							"rgb(166, 28, 0)", "rgb(204, 0, 0)", "rgb(230, 145, 56)", "rgb(241, 194, 50)", "rgb(106, 168, 79)",
+							"rgb(69, 129, 142)", "rgb(60, 120, 216)", "rgb(61, 133, 198)", "rgb(103, 78, 167)", "rgb(166, 77, 121)",
+							"rgb(91, 15, 0)", "rgb(102, 0, 0)", "rgb(120, 63, 4)", "rgb(127, 96, 0)", "rgb(39, 78, 19)", 
+							"rgb(12, 52, 61)", "rgb(28, 69, 135)", "rgb(7, 55, 99)", "rgb(32, 18, 77)", "rgb(76, 17, 48)"]
+						]
+					});
+
 				});
 			</script>
         ';
