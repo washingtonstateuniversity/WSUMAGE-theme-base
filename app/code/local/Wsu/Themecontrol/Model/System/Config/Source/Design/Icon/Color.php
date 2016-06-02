8cfbc4f97074a06f40372c030b16dd00c1d60330
Jeremy Bass <jeremybass@cableone.net>
2016-06-01 21:18:20 -0700

    move more out of wsu_wsu

--- app/code/local/Wsu/Themecontrol/Model/System/Config/Source/Design/Icon/Color.php
+++ app/code/local/Wsu/Themecontrol/Model/System/Config/Source/Design/Icon/Color.php
@@ -1,6 +1,6 @@
 <?php
 
-class Wsu_Wsu_Model_System_Config_Source_Design_Icon_Color{
+class Wsu_Themecontrol_Model_System_Config_Source_Design_Icon_Color{
     public function toOptionArray(){
 		return array(
 			array('value' => 'b',		'label' => Mage::helper('wsu_themecontrol')->__('Black')),
