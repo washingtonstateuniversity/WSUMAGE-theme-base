b0957a8899a14458111d8a8147a6cb5fa3050592
Jeremy Bass <jeremybass@cableone.net>
2017-01-09 12:40:22 -0800

    repair missing logic block

--- .src/app/code/local/Wsu/Themecontrol/Helper/Layout.php
+++ .src/app/code/local/Wsu/Themecontrol/Helper/Layout.php
@@ -120,7 +120,77 @@ class Wsu_Themecontrol_Helper_Layout extends Mage_Core_Helper_Abstract
         }
         return $class;
     }
+    public function getLayoutOptions($ref)//, $extract = array())
+    {
+        $BlockName = str_replace('.', '_', $ref->getNameInLayout());
+        $ControllerName = $ref->getRequest()->getControllerName();
+        $ActionName = $ref->getRequest()->getActionName();
+        $RouteName = $ref->getRequest()->getRouteName();
+        $ModuleName = $ref->getRequest()->getModuleName();
+// @codingStandardsIgnoreStart
+        $fullpath = $RouteName.'_'.$ControllerName.'_'.$ActionName;
+        echo "<!--\r";
+        echo "route :";
+        var_dump($fullpath);
+        echo "\rblock:";
+        var_dump($BlockName);
+        echo "\r-->";
+        $theme = $ref->helper('wsu_themecontrol');
 
+        $layouts = [
+
+            "product_info"=>[
+                    "product-content"=>["type"=>"flex-column","size"=>"fifths-3","order"=>"order-1"],
+                    "product-media"=>["size"=>"fifths-2","order"=>"order-2"],
+                    "media-block"=>["type"=>"flex-row"],
+
+                    "email_area"=>["order"=>"order-8"],
+                    "review_area"=>["order"=>"order-9"],
+                    "alert_urls_area"=>["order"=>"order-7"],
+                    "product_type_data_area"=>["order"=>"order-3"],
+                    "tier_price_area"=>["order"=>"order-4"],
+                    "extrahint_area"=>["order"=>"order-5"],
+                    "short_description_area"=>["order"=>"order-1"],
+                    "other_area"=>["order"=>"order-6"],
+                    "container1_area"=>["order"=>"order-2"],
+                    "description_area"=>["order"=>"order-10"],
+
+                ],
+            "product_info_media"=>[
+
+                    "product-image"=>["size"=>"sixths-5","order"=>"order-2"],
+                    "more-views"=>["size"=>"sixths-1","order"=>"order-1"],
+                    "more-views-imgs"=>["type"=>"flex-column"]
+                ],
+            "product_list"=>[
+                    "category-products-grid"=>["type"=>"flex-row","spacing"=>"justify-between"],
+                    "products-grid-item"=>["type"=>"flex-column"],
+                    "products-grid-item-image"=>["order"=>"order-1"],
+                    "products-grid-item-name"=>["order"=>"order-2"],
+                    "products-grid-item-price"=>["order"=>"order-4"],
+                    "products-grid-item-rating"=>["order"=>"order-3"],
+                    "products-grid-item-action"=>["order"=>"order-5"],
+                ],
+            "product_list_toolbar"=>[
+                    "toolbox-top"=>["type"=>"flex-row", "flow"=>"wrap"],
+                    "toolbox-bottom"=>["type"=>"flex-row", "flow"=>"wrap"]
+                ],
+
+
+
+        ];
+
+
+        $tmp = [
+
+                ];
+
+
+
+
+
+        return isset($layouts[$BlockName]) ? $layouts[$BlockName] : $tmp;
+    }
 
     public function getMapBlockMapping($block, $values)
     {
